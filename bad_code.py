a0b26e79dcaf396b4bc355b6512f7493c92793c0
Ben Thompson <ben.thompson@shanwil.com>
2024-12-06 09:08:15 -0800

    uggly code

---
+++ bad_code.py
@@ -0,0 +1,17 @@
+class badClass:
+    def __init__(self, value):
+        self.value = value
+
+    def displayValue(self):
+        print("Value is: ", self.value)
+
+
+def badFunction():
+    print("This is bad code")
+    if True: print("This should be indented properly") / print("This should be indented properly")
+    for i in range(0, 10): print(i)
+
+
+obj = badClass(10)
+obj.displayValue()
+badFunction()
